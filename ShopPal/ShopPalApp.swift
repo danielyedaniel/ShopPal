2f5513b6de816b657cf71d13b54167a22e64f2c3
Daniel <yedaniel99@gmail.com>
2022-12-25 22:17:36 -0500

    Initial Commit

---
+++ ShopPal/ShopPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopPalApp.swift
+//  ShopPal
+//
+//  Created by Daniel Ye on 2022-12-25.
+//
+
+import SwiftUI
+
+@main
+struct ShopPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShopPalUITests/ShopPalUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ShopPalUITestsLaunchTests.swift
+//  ShopPalUITests
+//
+//  Created by Daniel Ye on 2022-12-25.
+//
+
+import XCTest
+
+final class ShopPalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
