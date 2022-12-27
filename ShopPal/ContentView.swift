f9a5ec2b62b3d68c9c4e435d90d8915bbc6b5f9f
Daniel <yedaniel99@gmail.com>
2022-12-27 12:51:30 -0500

    commenting on ui components

--- ShopPal/ContentView.swift
+++ ShopPal/ContentView.swift
@@ -1,39 +1,50 @@
-//
-//  ContentView.swift
-//  ShopPal
-//
-//  Created by Daniel Ye on 2022-12-25.
-//
+
 
 import SwiftUI
 
 struct LoginView: View {
+    
+    //Variables to store input field data
     @State private var username: String = ""
     @State private var password: String = ""
     @State private var showCreateUserScreen: Bool = true
 
     var body: some View {
+        //Navigation view for the new user page
         NavigationView {
+            
+            //Geometry Reader to bind elements to certain margins of parent view
             GeometryReader { metrics in
+                
+                //Vstack containing all views
                 VStack (alignment: .center){
+                    
+                    //Title
                     Text("Login:")
                         .padding(20)
                         .font(.system(.title, design: .monospaced))
+                    
+                    //Username field
                     TextField("Username", text: $username)
                         .textFieldStyle(.roundedBorder)
                         .padding(5)
                         .frame(width: metrics.size.width*0.85,alignment: .center)
+                    
+                    //Password field
                     SecureField("Password", text: $password)
                         .textFieldStyle(.roundedBorder)
                         .padding(5)
                         .frame(width: metrics.size.width*0.85,alignment: .center)
+                    
+                    //Login button (currently holds no purpose)
                     Button(action: login) {
                         Text("Login")
                     }
                     .buttonStyle(RoundedButtonStyle())
                     .frame(height:20)
-                    
                     .padding()
+                    
+                    //New user creation button
                     NavigationLink(destination: CreateUserScreen()) {
                         Text("New User")
                     }
@@ -50,6 +61,7 @@ struct LoginView: View {
     }
 }
 
+//Button styling method
 struct RoundedButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
@@ -62,6 +74,7 @@ struct RoundedButtonStyle: ButtonStyle {
     }
 }
 
+//New user screen
 struct CreateUserScreen: View {
     @State private var username: String = ""
     @State private var password: String = ""
@@ -87,23 +100,7 @@ struct CreateUserScreen: View {
     }
 }
 
-
-
-
-
-
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-//    }
-//}
-
+//Main
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
