24cf5c9217854d03f6be3fab2b301458a5dedb21
Evan Wang <exwang@uwaterloo.ca>
2022-12-28 23:18:38 -0500

    login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -45,7 +45,26 @@ router.post("/signup", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
+    const schema = Joi.object({
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "org", "edu"] } })
+            .required()
+            .trim(),
+        password: Joi.string().min(8).trim(),
+    });
+    const { error } = schema.validate({ email: req.body.email, password: req.body.password });
+    if (error) return res.status(400).json(error.details);
 
+    const user = (await ddbClient.get({TableName: "ShopPal", Key: { email: req.body.email, receiptDate: "profile" }}).promise())
+    if (Object.keys(user).length == 0) return res.json("Wrong email or password")
+
+    const validPassword = await bcrypt.compare(req.body.password, user.Item.password);
+    if (!validPassword) return res.status(400).json("Wrong email or password");
+
+    delete user.Item.password;
+    delete user.Item.receiptDate;
+
+    res.status(200).json(user.Item);
 });
 
 module.exports = router;
