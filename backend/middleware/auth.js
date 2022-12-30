18e4c5c44adf472a5b703a677fbb56098492935a
Evan Wang <exwang@uwaterloo.ca>
2022-12-29 19:14:38 -0500

    added authentication middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,16 +1,24 @@
 const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const ddbClient = require("../dynamo");
 
-const auth = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
+const auth = async (req, res, next) => {
+    const schema = Joi.object({
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "org", "edu"] } })
+            .required()
+            .trim(),
+        password: Joi.string().min(8).trim(),
+    });
+    const { error } = schema.validate({ email: req.body.email, password: req.body.password });
+    if (error) return res.status(400).json(error.details);
 
-    if (!email || !password) {
-        const err = new Error("Email and password are required.");
-        err.status = 400;
-        return next(err);
-    }
+    const user = (await ddbClient.get({TableName: "ShopPal", Key: { email: req.body.email, receiptDate: "profile" }}).promise())
+    if (Object.keys(user).length == 0) return res.status(400).json("Wrong email or password")
 
-    // Do something to check if the user is authenticated
+    const validPassword = await bcrypt.compare(req.body.password, user.Item.password);
+    if (!validPassword) return res.status(400).json("Wrong email or password");
+
     next();
 }
 
--- backend/routes/receipt.js
+++ backend/routes/receipt.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/add", async (req, res) => {
-
+    return res.json("added")
 });
 
 router.post("/delete", async (req, res) => {
