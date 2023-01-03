bbbde6d37450faede37e263e2226affa10ba3a13
Kevin Liu <kevinliu676@yahoo.ca>
2023-01-02 22:26:31 -0500

    Revert "started on database stuff"
    This reverts commit 60a9eb0d099e173755075df7932831d71d162097.

--- ShopPal/ContentView.swift
+++ ShopPal/ContentView.swift
@@ -165,16 +165,7 @@ struct SignUpView: View {
                 
                 Button(action: {
                     if(password == confirmPassword) {
-                        let accountDetails = NewAccount(firstName: firstName, lastName: lastName, email: email, password: password)
-                        let postRequest = APIRequest(endpoint: "accounts")
-                        postRequest.save(accountDetails, completion: { result in
-                            switch result {
-                            case .success(let account):
-                                print("Success \(account.firstName)")
-                            case .failure(let error):
-                                print("An error occured \(error)")
-                            }
-                        })
+                        print("Success")
                     }
                     else {
                         print("Fail")
