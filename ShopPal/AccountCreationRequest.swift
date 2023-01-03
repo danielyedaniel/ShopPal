09aca14faf0816170fd5d02e7ad8e6da79f208d2
Kevin Liu <kevinliu676@yahoo.ca>
2023-01-02 21:04:45 -0500

    launch screen addition attempt #2

---
+++ ShopPal/AccountCreationRequest.swift
@@ -0,0 +1,24 @@
+//
+//  AccountCreationRequest.swift
+//  ShopPal
+//
+//  Created by Kevin Liu on 2023-01-02.
+//
+
+import Foundation
+
+enum APIError:Error {
+    case responseProblem
+    case decodingProblem
+    case otherProblem
+}
+
+/*
+struct AccountCreationRequest {
+    let resourceURL: URL
+    
+    init(endpoitn: String) {
+        // let resourceString = "
+    }
+}
+*/
