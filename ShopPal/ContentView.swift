5912ab2d7b74e3edce18ca14db5ed3916d2ae68b
Daniel <yedaniel99@gmail.com>
2023-01-04 12:10:45 -0500

    formatting for the homeview

--- ShopPal/ContentView.swift
+++ ShopPal/ContentView.swift
@@ -398,7 +398,6 @@ struct mainScreen: View {
 //Home Screen
 struct HomeView: View {
     let groceryList = ["Apples", "Bananas", "Oranges", "Strawberries"]
-
     @State private var checked = [Bool](repeating: false, count: 4)
 
     var body: some View {
@@ -406,16 +405,52 @@ struct HomeView: View {
             ForEach(groceryList, id: \.self) { grocery in
                 HStack {
                     Text(grocery)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .multilineTextAlignment(.leading)
+                        .font(.system(size: 20, weight: .medium, design: .default))
+                        .padding(.vertical, 12)
+                        .padding(.horizontal, 16)
+                        .foregroundColor(.white)
+                        .padding(.leading)
+                        .padding(.trailing)
+                        .scaledToFit()
                     Spacer()
                     Button(action: {
                         self.checked[self.groceryList.firstIndex(of: grocery)!] = !self.checked[self.groceryList.firstIndex(of: grocery)!]
                     }) {
                         Image(systemName: self.checked[self.groceryList.firstIndex(of: grocery)!] ? "checkmark.square" : "square")
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .multilineTextAlignment(.leading)
+                            .font(.system(size: 20, weight: .medium, design: .default))
+                            .padding(.vertical, 12)
+                            .padding(.horizontal, 16)
+                            .foregroundColor(.white)
+                            .padding(.leading)
+                            .padding(.trailing)
+                            .scaledToFit()
                     }
                 }
             }
+            .overlay(border, alignment: .top)
         }
+        .ignoresSafeArea()
+        .background(Color(red: 0.06, green: 0.06, blue: 0.06))
+        
     }
+    var border: some View {
+        RoundedRectangle(cornerRadius: 16)
+          .strokeBorder(
+            LinearGradient(
+              gradient: .init(
+                colors: [
+                    Color(red: 0.08, green: 0.64, blue: 0.15)
+                ]
+              ),
+              startPoint: .topLeading,
+              endPoint: .bottomTrailing
+            )
+          )
+      }
 }//End of home screen
 
 //Settings
