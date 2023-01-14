d858bf31aeee5f55f4bab798e9f3d25666db6187
John Turco <john7turco@gmail.com>
2023-01-14 17:10:55 -0500

    Temproary build failed fix

--- ShopPal/MainTabs/Chart.swift
+++ ShopPal/MainTabs/Chart.swift
@@ -11,36 +11,9 @@ import SwiftUI
 struct ChartView: View {
     var body: some View {
         
-        let chartView = BarChartView()
-        chartView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
-        view.addSubview(chartView)
-
-        let dataEntries = [BarChartDataEntry(x: 1.0, y: 20.0),
-                           BarChartDataEntry(x: 2.0, y: 4.0),
-                           BarChartDataEntry(x: 3.0, y: 6.0),
-                           BarChartDataEntry(x: 4.0, y: 3.0),
-                           BarChartDataEntry(x: 5.0, y: 12.0),
-                           BarChartDataEntry(x: 6.0, y: 16.0)]
-        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Bar Chart")
-        let chartData = BarChartData(dataSet: chartDataSet)
-        chartView.data = chartData
-//        VStack{
-//            Spacer()
-//            Text("Monthly Spending")
-//                .foregroundColor(.green)
-//                .font(.system(size: 40, weight: .heavy, design: .default))
-//            Text("Report")
-//                .foregroundColor(.green)
-//                .font(.system(size: 40, weight: .heavy, design: .default))
-//            Spacer()
-//            BarGraph(data: [("A", 40.0), ("B", 30.0), ("C", 20.0), ("D", 10.0)])
-//            Spacer()
-//            Spacer()
-//            Spacer()
-//            Spacer()
-//            Spacer()
-//        }
-        
+        VStack{
+            
+        }
         
     }
 }
